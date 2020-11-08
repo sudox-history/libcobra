cdb36bfee2781ad57a7ee60b2d1aaf4dfafa8f5e
undefined7887 <undefined7887@gmail.com>
2020-11-08 20:59:52 +0300

    [FIX] Fixed headers

--- include/cobra.h
+++ include/cobra.h
@@ -1,7 +1,15 @@
 #ifndef COBRA_H
 #define COBRA_H
 
+#ifdef cplusplus
+extern "C" {
+#endif
+
 #include "internal/tcp_connection.h"
 #include "internal/buffer.h"
+
+#ifdef cplusplus
+}
+#endif
 
 #endif //COBRA_H
