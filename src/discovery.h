d7e244790ef0563901f4566936781a6c4fb737c0
undefined7887 <undefined7887@gmail.com>
2020-11-18 23:19:39 +0300

    [BACKUP]

--- src/discovery.h
+++ src/discovery.h
@@ -13,13 +13,15 @@
 #define COBRA_DISCOVERY_OK 0
 #define COBRA_DISCOVERY_ERR_ALREADY_STARTED 1
 #define COBRA_DISCOVERY_ERR_BINDING 2
-#define COBRA_DISCOVERY_ERR_SENDING 3
+#define COBRA_DISCOVERY_ERR_STARTING_BROADCAST 3
+#define COBRA_DISCOVERY_ERR_SENDING 4
 
 #ifdef COBRA_DISCOVERY_PRIVATE
 uint8_t COBRA_DISCOVERY_PACKET[] = {8, 100, 193, 210, 19};
 #define COBRA_DISCOVERY_ANY_ADDR "0.0.0.0"
 #define COBRA_DISCOVERY_BROADCAST_ADDR "239.255.255.250"
 #define COBRA_DISCOVERY_PORT 55669
+#define COBRA_DISCOVERY_TIMEOUT 5000
 #endif
 
 typedef struct cobra_discovery_t cobra_discovery_t;
@@ -34,15 +36,17 @@ typedef void (*cobra_discovery_close_cb)
 struct cobra_discovery_t {
     /* Libuv handles */
     uv_loop_t loop;
-    uv_timer_t timer_handle;
     uv_udp_t udp_handle;
+    uv_timer_t timer_handle;
 
     /* Management */
-    cobra_buffer_t listener_read_buffer;
     bool is_listening;
     bool is_scanning;
     bool is_closing;
     int close_error;
+
+    /* Read */
+    cobra_buffer_t read_buffer;
 
     /* Callbacks */
     cobra_discovery_found_cb on_found;
